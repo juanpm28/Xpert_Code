f22e4f59a1ace571e17d1cc1f68d16be435e575f
dmg <davidmunozg.03@gmail.com>
2022-07-05 23:22:22 -0500

    carrito de compras

--- VengadoresDeLaLimpieza/src/js/Contactanos.js
+++ VengadoresDeLaLimpieza/src/js/Contactanos.js
@@ -1,3 +1,4 @@
+const taquito= false;
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 const expresiones = {
@@ -26,26 +27,40 @@ const validarFormulario = (e) => {
         case "mensaje":
 			validarCampo(expresiones.mensaje, e.target, 'mensaje');
 		break;
+		
 	}
 }
+
 const validarCampo = (expresion, input, campo) => {
-	if(expresion.test(input.value)){
+	if(expresion.test(input.value)) {
 		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto'); 
 		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
 		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo'); 
 		campos[campo] = true;
-	} else {
+		taquito=true;
+}
+	else {
 		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
 		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
 		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
 		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
 		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
 		campos[campo] = false;
-	}
-} //puntero del mouse
-inputs.forEach((input) => {
+		taquito=false;
+}}//puntero del mouse
+inputs.forEach((input) => { 
 	input.addEventListener('keyup', validarFormulario);
 	input.addEventListener('blur', validarFormulario);
 });
+
+function mostrar(){
+if(taquito=false){
+	swal("Error", "Por favor llena correctamente","error");
+}else {
+	swal("Correcto", "Enviado correctamente","success");
+}}
+// function mostrar(){
+//  }
+
--- VengadoresDeLaLimpieza/src/js/Productos.js
+++ VengadoresDeLaLimpieza/src/js/Productos.js
@@ -1,41 +1,41 @@
 const productosLimpieza = [{"nombre" : "Kit de Limpieza Completo Mascota Feliz",
-"img" : "/src/imagenes/1_kit1_EDITADO.jpg",
+"img" : "/src/imagenes/1_kit.jpg",
 "precio" : "587.00",
 "sku" : "DW195B3"}, 
 {"nombre" : "Scotch-Brite Guantes de Cocina, Medianos",
-"img" : "/src/imagenes/2_guant1_EDITADO.jpg",
+"img" : "/src/imagenes/2_guantes.jpg",
 "precio" : "31.82",
 "sku" : "MMS195S9"}, 
 {"nombre" : "Scotch-Brite Paño Microfibra de Colores",
-"img" : "/src/imagenes/3_trapo1_EDITADO.jpg",
+"img" : "/src/imagenes/3_microfibra.jpg",
 "precio" : "13.00",
 "sku" : "JG953M4"}, 
 {"nombre" : "Trapeador Plano Atrapa Polvo Microfibra",
-"img" : "/src/imagenes/4_trapeador1_EDITADO.jpg",
+"img" : "/src/imagenes/4_trapeadorazul.jpg",
 "precio" : "249",
 "sku" : "LM347G1"}, 
 {"nombre" : "Recogedor de Basura Rojo Multiusos",
-"img" : "/src/imagenes/5_recoge1_EDITADO.jpg",
+"img" : "/src/imagenes/5_recogedor.jpg",
 "precio" : "200.00",
 "sku" : "PPV110T7"}, 
 {"nombre" : " Kit Escoba Exterior y Atrapa Pelo ",
-"img" : "/src/imagenes/6_escoba1_EDITADO.jpg", 
+"img" : "/src/imagenes/6_escoba.jpg", 
 "precio" : "169.00", 
 "sku" : "WPD477S3"}, 
 {"nombre" : "Scotch-Brite Trapeador Extra Largo",
-"img" : "/src/imagenes/7_traplargo1_EDITADO.jpg",
+"img" : "/src/imagenes/7_trapeadorlargo.jpg",
 "precio" : "115.20",
 "sku" : "MFL024B1"}, 
 {"nombre" : "Stainless Steel Toallas 30CT Limpieza Profunda",
-"img" : "/src/imagenes/8_toallas1_EDITADO.jpg",
+"img" : "/src/imagenes/8_toallas.jpg",
 "precio" : "240.00",
 "sku" : "JPE94Y5"}, 
 {"nombre" : "Jalador más Paño Suelos con Microfibra", 
-"img" : "/src/imagenes/9_jalador1_EDITADO.jpg",
+"img" : "/src/imagenes/9_jalador.jpg",
 "precio" : "121.00",
 "sku" : "JLR545T6"}, 
 {"nombre" : "Cubeta 20 litros Exprimidor y Separador de Agua",
-"img" : "/src/imagenes/10_cubeta1_EDITADO.jpg",
+"img" : "/src/imagenes/10_cubeta.jpg",
 "precio" : "1,260.00",
 "sku" : "RKQ35H1"}];
 
@@ -43,7 +43,7 @@ function addItem(item, con){
     
     const itemHTML= `<div class="card-deck">
     <div class="card" style="widt h: 18rem;">
-    <img class="cont-imagen" src="${item.img}" class="card-img-top"  alt="..." >
+    <img class="cont-imagen" src="${item.img}" class="card-img-top"  alt="LimpiaClean" >
     <div class="card-body">
     <div id="cinco-estrellas">
     <!--5 estrellas-->
@@ -67,7 +67,7 @@ function addItem(item, con){
     <button onclick="press(3` + con + `,0)" class="btn btn-primary btnRed"><b>-</b></button>
     <input id="3` + con + `" type="text" class="contador" style="text-align: center;" value="" placeholder="0">
     </div>
-    <button type="button" class="btn btn-primary btn-lg btn-block btnAgregar">Agregar</button>
+    <a href="https://www.paypal.com/webapps/shoppingcart/error?flowlogging_id=f116914a394fa&code=GENERIC_ERROR&mfid=1656979172865_f116914a394fa" onclick="mostrar()"><button type="button" class="btn btn-primary btn-lg btn-block btnAgregar" onclick="mostrar()">Comprar</button></a>
     </div>
 </div>
 </div>
@@ -111,7 +111,9 @@ function press(id_input, operacion){
         numero--; 
     }
     $("#"+id_input).val(numero);
-  }
+}
   
+function mostrar(){
+    swal("Procesando VengaPago", "Procesando VengaPago", "succes"); 
+}
 
-  
