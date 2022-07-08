471007d86fa3ce28713c8e083bb431e3e96e5f52
mariafelixgarcia <mariafelixgarciatellez@gmail.com>
2022-07-07 23:16:25 -0500

    Correcciones de segunda retro

--- src/js/Contactanos.js
+++ src/js/Contactanos.js
@@ -1,5 +1,6 @@
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
+
 const expresiones = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]{3,60}$/, // Letras y espacios, pueden llevar acentos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
@@ -29,7 +30,7 @@ const validarFormulario = (e) => {
 	}
 }
 const validarCampo = (expresion, input, campo) => {
-	if(expresion.test(input.value)){
+	if(expresion.test(input.value.trim())){
 		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
 		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
 		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
@@ -45,7 +46,37 @@ const validarCampo = (expresion, input, campo) => {
 		campos[campo] = false;
 	}
 } //puntero del mouse
+/* let input = document.querySelectorAll('input');
+input.addEventListener('input',function(){
+     this.value = this.value.trim();
+}) */
+
 inputs.forEach((input) => {
 	input.addEventListener('keyup', validarFormulario);
 	input.addEventListener('blur', validarFormulario);
 });
+
+let btnContacto = document.getElementById("btnContacto");
+
+btnContacto.addEventListener("click", (event)=> {
+    event.preventDefault();
+    if(campos.nombre && campos.correo && campos.telefono && campos.mensaje){
+        Swal.fire({
+            icon: 'success',
+            title: '¡Tu mensaje ha sido enviado a nuestros Vengadores!',
+            showConfirmButton: false,
+            timer: 3500
+          })
+       window.location='../pages/Contactanos.html';
+       return;
+    }
+    else{ 
+    Swal.fire({
+        icon: 'error',
+        title: 'Por favor, rellena los campos correctamente',
+        text: 'Intenta de nuevo',
+
+      });
+      return;
+    }
+}); 
